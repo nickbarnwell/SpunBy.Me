560e25c77544f4e6208f98f39fb08076ceb02220
Ryan Ewing <ewing.ryan@gmail.com>
2011-10-15 03:46:11 -0700

    adding ryan's content

---
+++ SpunByMe/desktop/static/js/dashboard.js
@@ -0,0 +1,45 @@
+/*
+       * Change out the video that is playing
+       */
+      
+      // Update a particular HTML element with a new value
+      function updateHTML(elmId, value) {
+        document.getElementById(elmId).innerHTML = value;
+      }
+      
+      // Loads the selected video into the player.
+      function loadVideo() {
+        var selectBox = document.getElementById("videoSelection");
+        var videoID = selectBox.options[selectBox.selectedIndex].value
+        
+        if(ytplayer) {
+          ytplayer.loadVideoById(videoID);
+        }
+      }
+      
+      // This function is called when an error is thrown by the player
+      function onPlayerError(errorCode) {
+        alert("An error occured of type:" + errorCode);
+      }
+      
+      // This function is automatically called by the player once it loads
+      function onYouTubePlayerReady(playerId) {
+        ytplayer = document.getElementById("ytPlayer");
+        ytplayer.addEventListener("onError", "onPlayerError");
+      }
+      
+      // The "main method" of this sample. Called when someone clicks "Run".
+      function loadPlayer(videoID) {
+        // Lets Flash from another domain call JavaScript
+        var params = { allowScriptAccess: "always" };
+        // The element id of the Flash embed
+        var atts = { id: "ytPlayer" };
+        // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
+        swfobject.embedSWF("http://www.youtube.com/v/" + videoID + 
+                           "?version=3&enablejsapi=1&playerapiid=player1", 
+                           "videoDiv", "480", "295", "9", null, null, params, atts);
+      }
+      function _run() {
+        loadPlayer("ylLzyHk54Z0");
+      }
+      google.setOnLoadCallback(_run);
