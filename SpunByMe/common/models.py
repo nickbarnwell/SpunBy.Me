5129c32bbb6df70fb4f162dab27e3f56d12e531e
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-10-15 05:53:20 -0700

    Fix queue/confidence #2

--- SpunByMe/common/models.py
+++ SpunByMe/common/models.py
@@ -133,7 +133,7 @@ class QueueData(models.Model):
     if self.upvotes - self.downvotes == 0:
       return 0
     else:
-      return self._confidence(ups, downs)
+      return self._confidence(self.upvotes, self.downvotes)
   
   def __unicode__(self):
     return "Party: %s -- Song: %s" % (self.party, self.song)
