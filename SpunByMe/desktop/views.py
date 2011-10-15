d184b42f2799e2499330b5f00ea447f7c2183e43
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-10-15 06:26:58 -0700

    fb auth try#2

--- SpunByMe/desktop/views.py
+++ SpunByMe/desktop/views.py
@@ -10,7 +10,7 @@ FACEBOOK_API_SECRET = '794cb30ba61fb6609bdd81a9b61eead2'
 OAUTH_REDIRECT_URI = 'http://spunbyme.herokuapp.com/login/'
 
 def index(request):
-  if request.session.get('user', None) is None:
+  if request.session.get('access_token', None) is None:
     context = RequestContext(request, {
       'login_url': 'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s' \
         % (FACEBOOK_APP_ID, OAUTH_REDIRECT_URI)
@@ -39,7 +39,7 @@ def login(request):
     print type(access_token)
     me = cjson.decode(
       urllib2.urlopen(
-        'https://graph.facebook.com/me/?access_token=%s' % access_token))
+        'https://graph.facebook.com/me/?access_token='+access_token))
     user = User()
     user.first_name = me['first_name']
     user.last_name = me['last_name']
