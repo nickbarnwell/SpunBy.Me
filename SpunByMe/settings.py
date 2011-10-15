11d686e7dacd47d498df27b14ed2a92995f5960a
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-10-14 20:08:39 -0700

    Switch to SQLite3 for development

--- SpunByMe/settings.py
+++ SpunByMe/settings.py
@@ -1,4 +1,5 @@
 # Django settings for SpunByMe project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '%s/dev.db' % os.getcwd(),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
