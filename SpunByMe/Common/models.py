3bcab929b6b82732a40bf7e2fbbfd5b4abd99c9e
Bill Cauchois <wcauchois@gmail.com>
2011-10-14 22:34:19 -0700

    fix whitespace

--- SpunByMe/Common/models.py
+++ SpunByMe/Common/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from party.models import Party
 # Create your models here.
 class Song(models.Model):
- title = models.CharField(blank=False)
- artist = models.CharField(blank=False)
- swf_url = models.CharField(blank=False)
+  title = models.CharField(blank=False)
+  artist = models.CharField(blank=False)
+  swf_url = models.CharField(blank=False)
 
 class SongQueue(models.Model):
   songs = models.ManyToManyField(Song, through='QueueData')
