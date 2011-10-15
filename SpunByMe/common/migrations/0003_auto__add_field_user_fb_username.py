92046a31be4a919e1e508998f0b598a71e77335d
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-10-15 06:35:22 -0700

    Add migration for FB users

---
+++ SpunByMe/common/migrations/0003_auto__add_field_user_fb_username.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'User.fb_username'
+        db.add_column('common_user', 'fb_username', self.gf('django.db.models.fields.CharField')(default='old_username', max_length=255), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'User.fb_username'
+        db.delete_column('common_user', 'fb_username')
+
+
+    models = {
+        'common.party': {
+            'Meta': {'object_name': 'Party'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'songs': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Song']", 'through': "orm['common.QueueData']", 'symmetrical': 'False'})
+        },
+        'common.queuedata': {
+            'Meta': {'object_name': 'QueueData'},
+            'added_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'downvotes': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Party']"}),
+            'song': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Song']"}),
+            'upvotes': ('django.db.models.fields.IntegerField', [], {'default': '1'})
+        },
+        'common.song': {
+            'Meta': {'object_name': 'Song'},
+            'artist': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'video_id': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'common.user': {
+            'Meta': {'object_name': 'User'},
+            'fb_username': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['common']
