b1751bc81712ce9221882661990818ce7b27ecfa
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-10-14 22:56:01 -0700

    Models more or less finished off

--- SpunByMe/Party/models.py
+++ SpunByMe/Party/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from common.models import *
 import datetime
 # Create your models here.
 
 class Party(models.Model):
   name = models.CharField(max_length=50)
   created_at = models.DateTimeField(auto_now_add=True, blank=False)
+  queue = models.OneToOneField(SongQueue, blank=False, on_delete=models.CASCADE)
   @property
   def expired(self):
     return datetime.datetime.now() >= self.created_at+datetime.timedelta(24)
