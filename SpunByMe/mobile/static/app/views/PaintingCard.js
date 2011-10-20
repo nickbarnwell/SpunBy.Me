3ba2e7b69624239a8ed2790287daf8d56b8d4214
Alex Juarez <Couatli@gmail.com>
2011-10-20 14:33:13 -0700

    Changed album art

--- SpunByMe/mobile/static/app/views/PaintingCard.js
+++ SpunByMe/mobile/static/app/views/PaintingCard.js
@@ -5,7 +5,7 @@
         imageCard = {
             id:  'image_' + pane.slug,
             cls: 'painting ' + pane.slug,
-            html: '<div style="display: block; height: 100%; background-image: url(\'"' + pane.albumart + '\')" />'
+            html: '<div style="display: block; height: 100%; background-image: url(\'' + pane.albumart + '\')" />'
         },
 
         infoCard = {
